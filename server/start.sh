df94846fa476706f293366984dba1e293e2eef71
brisandbeans <bdougsand+cfb@gmail.com>
2015-07-29 17:36:11 -0400

    Daemonize Django

--- server/start.sh
+++ server/start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-
 service postgresql start
 
-cd /app
-python manage.py runserver 0.0.0.0:3000
+pid_file=/app/_citydash_django.pid
+server_out=/app/django.log
+server_err=/app/error.log
+
+echo $$ >$pid_file
+
+if [ -f $pid_file ]; then
+    pid=$(cat $pid_file);
+    echo "Killing server ($pid)";
+    kill -KILL $(cat $pid_file);
+    rm -f $pid_file
+fi;
+
+
+nohup python /app/manage.py runserver 0.0.0.0:3000 >$server_out 2>$server_err&
