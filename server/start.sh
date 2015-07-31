bacf7490557b17cebcc129d42f9ce00f1bf0ebde
brisandbeans <bdougsand+cfb@gmail.com>
2015-07-31 16:42:15 -0400

    Fixed start script

--- server/start.sh
+++ server/start.sh
@@ -5,8 +5,6 @@ pid_file=/app/_citydash_django.pid
 server_out=/app/django.log
 server_err=/app/error.log
 
-echo $$ >$pid_file
-
 if [ -f $pid_file ]; then
     pid=$(cat $pid_file);
     echo "Killing server ($pid)";
@@ -14,5 +12,5 @@ if [ -f $pid_file ]; then
     rm -f $pid_file
 fi;
 
-
 nohup python /app/manage.py runserver 0.0.0.0:3000 >$server_out 2>$server_err&
+echo $! >$pid_file
