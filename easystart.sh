8eb82e8ad72ee4ab9b10fc7ab0043ccc4c3c4df4
brisandbeans <bdougsand+cfb@gmail.com>
2015-08-05 20:33:31 -0400

    easystart.sh modifications
    Running ./easystart.sh -x will stop the running container, if applicable
    Optional argument to script will run inside the container

--- easystart.sh
+++ easystart.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-RUN_COMMAND="/bin/bash"
-POST_COMMAND=""
 HOST_PORT=3000
 CITYDASH_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 
 function setup_environment {
     eval $(boot2docker shellinit)
@@ -25,17 +22,23 @@ function report_changes {
 
 function print_help {
     echo "
-Quickly build and launch the CityDash container.
+Usage: ${BASH_SOURCE[0]} [options] [command]
+
+Quickly build and launch the CityDash container. If a command is
+specified, run that command in the container and exit.
 
 Options:
   -b      Force Docker to build a new image, even if one exists
   -B      Ignore changes to dependencies when determining whether
-          to build a new image
-  -p      Set up the Boot2Docker VM to forward the Django port to
+          to build a new image. Only build if there is no
+          existing image
+  -f      Set up the Boot2Docker VM to forward the Django port to
           localhost.
+  -p <port> Run on a port other than 3000
   -r      Force Docker to run a new container, rather than
           attach to one that is already running
   -s      Create a container and run the CityDash start script
+  -x      If a running container is found, stop it
 "
 }
 
@@ -43,8 +46,10 @@ SHOULD_BUILD=0
 FORCE_RUN=0
 VM_PORT_FORWARDING=0
 IGNORE_CHANGES=0
+AUTOSTART=0
+STOP_RUNNING=0
 
-while getopts ":rbBpsh" opt; do
+while getopts ":rbBfpshx" opt; do
     case $opt in
         b)
             SHOULD_BUILD=1
@@ -56,21 +61,42 @@ while getopts ":rbBpsh" opt; do
             FORCE_RUN=1
             ;;
         s)
+            AUTOSTART=1
             RUN_COMMAND="/bin/sh /app/start.sh"
             ;;
-        p)
+        f)
             VM_PORT_FORWARDING=1
+            ;;
+        p)
+            check_re='^[0-9]{4,}$'
+            if ! [[ $OPTARG =~ $check_re ]]; then
+                echo "Port must be an integer >=1000."
+                exit 1
+            fi
+            HOST_PORT=$OPTARG
+            ;;
+        x)
+            STOP_RUNNING=1
             ;;
         h)
             print_help
             exit 1
             ;;
+
         \?)
             echo "Unrecognized flag: -$OPTARG" >&2
             exit 1
             ;;
     esac
 done
+
+if [ -z "$RUN_COMMAND" ]; then
+    if [ -n "$1" ]; then
+        RUN_COMMAND="$*"
+    else
+        RUN_COMMAND=/bin/bash
+    fi
+fi
 
 if (! (which docker >/dev/null)); then
     # Docker is not installed
@@ -79,10 +105,9 @@ if (! (which docker >/dev/null)); then
     exit 1
 fi;
 
-
-
 # Using Boot2Docker?
 if (which boot2docker >/dev/null); then
+    USE_B2D=1
     # Is it up?
     if [ $(boot2docker status) != "running" ] ; then
         echo "Starting up Boot2Docker."
@@ -102,11 +127,12 @@ else
         open_browser "http://boot2docker.io"
         exit 1
     fi
+    USE_B2D=0
 fi;
 
 IMAGE_CREATED=$(docker inspect citydash | grep "Created" | perl -n -e'/"Created": "(\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d)\.\d+Z"/ && print $1')
 
-if ((!$IGNORE_CHANGES)); then
+if ((!IGNORE_CHANGES)); then
     # Determine if the image should be rebuilt.
     if ((!$SHOULD_BUILD)); then
         SHOULD_BUILD=1
@@ -135,17 +161,23 @@ if ((!$IGNORE_CHANGES)); then
     fi
 fi
 
-if (($SHOULD_BUILD)); then
+if ((SHOULD_BUILD)); then
     echo "Building citydash image."
     docker build -t citydash $CITYDASH_DIR
 fi;
 
-if ((!$FORCE_RUN)); then
+if ((!FORCE_RUN || STOP_RUNNING)); then
     # Determine if the container is already running:
     CONTAINER_ID=$(docker ps | awk '/citydash/ { if (match($2, /^citydash/)) print $1 }')
 else
     CONTAINER_ID=""
 fi;
+
+if [ -n $CONTAINER_ID ] && ((STOP_RUNNING)); then
+    echo "Stopping container: $CONTAINER_ID"
+    docker stop $CONTAINER_ID
+    CONTAINER_ID=""
+fi
 
 if [ -n "$CONTAINER_ID" ]; then
     # Found a container. Attach to it:
@@ -162,5 +194,37 @@ if [ $? -eq 1 ]; then
 fi;
 
 
+if ((AUTOSTART)); then
+    # Do a busy wait for the Django server to start up
+    wait_count=0
+    is_running=1
+    if ((USE_B2D)); then
+        HOSTNAME=$(boot2docker ip)
+    else
+        HOSTNAME="localhost"
+    fi
+    # Wait for the port to open
+    until (lsof -i @$HOSTNAME:$HOST_PORT >/dev/null); do
+        ((wait_count++))
 
-# Possible next steps:
+        if ((wait_count > 30)); then
+            is_running=0
+            break
+        fi
+
+        sleep 1
+    done
+
+    if ((is_running)); then
+        echo "Django started successfully on port $HOST_PORT."
+
+        if ((VM_PORT_FORWARDING || !USE_B2D)); then
+            open_browser "http://localhost:$HOST_PORT"
+        else
+            open_browser "http://$HOSTNAME:$HOST_PORT"
+        fi
+    else
+        echo "The Django server did not start within 30 seconds."
+        exit 3
+    fi
+fi
