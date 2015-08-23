6d09ca651a7d0a8493e331a74730ea0ba0d58652
brisandbeans <bdougsand+cfb@gmail.com>
2015-08-23 12:13:49 -0400

    Made server script slightly smarter
    - Detect the application root, either by using the APP_ROOT environment variable set when the Docker container is created or by using the directory in which start.sh is located.
    - Detect the application name through some (possibly ill-advised) hocus-pocus
    - Check if Django started successfully

--- server/start.sh
+++ server/start.sh
@@ -5,10 +5,19 @@ service redis-server start # Required for celery and caching
 # Force Celery to run as root:
 export C_FORCE_ROOT=1
 
-pid_file=/var/run/_citydash_django.pid
-server_out=/app/django.log
-server_err=/app/error.log
+if [ -z "$APP_ROOT" ]; then
+    export APP_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+fi
 
+if [ -z "$APP_NAME" ]; then
+    export APP_NAME=$(basename $(dirname $(find $APP_ROOT -name settings.py | head -n 1)))
+fi
+
+pid_file=/var/run/$APP_NAME_django.pid
+server_out=$APP_ROOT/django.log
+server_err=$APP_ROOT/error.log
+
+# The APP_PORT global is set
 if [ -z "$APP_PORT" ]; then
     export APP_PORT=3000
 fi
@@ -17,17 +26,36 @@ if [ -n "$1" ]; then
     port="$1"
 fi
 
-if [ -f $pid_file ]; then
-    pid=$(cat $pid_file);
-    echo "Killing server ($pid)";
-    kill -KILL $(cat $pid_file);
+if pid=$(cat $pid_file); then
+    if kill $pid 2>/dev/null; then
+        echo "Killed running server (pid: $pid)";
+    elif kill -0 $pid 2>/dev/null; then
+        echo "Could not kill the running server (pid: $pid)"
+        exit 1
+    fi
+
+    # Either we've killed the process, or the pid file was out of date
     rm -f $pid_file
 fi;
 
 # Prefer Python 3:
 PYTHON_BIN=$(which python3 || which python)
 echo "Applying any outstanding migrations"
-$PYTHON_BIN /app/manage.py migrate --database=migrate
+$PYTHON_BIN $APP_ROOT/manage.py migrate --database=migrate
 echo "Starting Django.  Logging output to: $(readlink -f $server_out)"
-nohup $PYTHON_BIN /app/manage.py runserver 0.0.0.0:$APP_PORT >$server_out 2>$server_err&
+nohup $PYTHON_BIN $APP_ROOT/manage.py runserver 0.0.0.0:$APP_PORT >$server_out 2>$server_err&
 echo $! >$pid_file
+
+sleep 1
+server_ping=$(curl -s "http://0.0.0.0:3000/ping")
+curl_code=$?
+
+if ((curl_code)); then
+    tail $server_err
+else
+    if [ "$server_ping" = "running" ]; then
+        echo "Django is now running on port $APP_PORT."
+    else
+        echo "The server is running, but there may be a configuration error."
+    fi
+fi
