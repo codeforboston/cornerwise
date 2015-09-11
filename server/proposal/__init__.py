539ea776176a0e824a75f550707e200e0750b00f
brisandbeans <bdougsand+cfb@gmail.com>
2015-09-11 17:35:04 -0400

    Fixed issue with importing before app was configured

--- server/proposal/__init__.py
+++ server/proposal/__init__.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
 from datetime import datetime
 
-from . import tasks
-
 class ProposalConfig(AppConfig):
     def ready(self):
+        from . import tasks
+
         tasks.scrape_reports_and_decisions.delay(datetime(2015, 6, 1))
