58c2737055e6cc4a6d236a604b9af3983dcf0dfe
brisandbeans <bdougsand+cfb@gmail.com>
2015-10-28 16:28:36 -0400

    Keyword phrases from text
    - Experiments with NLTK

---
+++ server/proposal/keywords.py
@@ -0,0 +1,50 @@
+import nltk
+
+def group_true(pred, l):
+    """Group adjacent elements in the collection l for which pred(element)
+    is true.
+
+    :param pred: A test function that will be run on each member of l
+    :param l: a collection
+
+    :returns: A generator that emits lists of elements from l
+    """
+    group = []
+
+    for elem in l:
+        if pred(elem):
+            group.append(elem)
+        elif group:
+            yield group
+            group = []
+
+    if group:
+        yield group
+
+def is_noun_phrase(tagged):
+    """
+    :param tagged: An iterable of tagged words (i.e., (word, tag) tuples)
+
+    :returns: boolean
+    """
+    return tagged[-1][1] == "NN"
+
+
+def noun_phrases(tagged):
+    groups =  group_true(lambda tw: tw[1] in set(["NN", "JJ"]), tagged)
+
+    return filter(is_noun_phrase, groups)
+
+def join_words(tagged):
+    return " ".join(tw[0] for tw in tagged)
+
+def keywords(text):
+    """
+    :returns: An iterable of strings containing the recognized
+    """
+    tokenized = nltk.word_tokenize(text)
+    tagged = nltk.pos_tag(tokenized)
+    return map(join_words, noun_phrases(tagged))
+
+if __name__ == "__main__":
+    do_main()
