cef0dadb000191da1b257a0b5ffdbe660be7a212
brisandbeans <bdougsand+cfb@gmail.com>
2015-10-28 15:19:01 -0400

    Minor rearrangements

--- server/proposal/cleanup.py
+++ server/proposal/cleanup.py
@@ -7,7 +7,7 @@ import logging, os, re
 from django.conf import settings
 
 from .models import Attribute, Document, Image
-from utils import extension
+from utils import extension, make_file_mover
 
 logger = logging.getLogger(__name__)
 
@@ -40,14 +40,20 @@ def remove_duplicate_attributes():
 
     Attribute.objects.filter(pk__in=ids).delete()
 
+do_rename = make_file_mover("document")
+
 def rename_document(doc):
     docpath = doc.document and doc.document.path
+    if not docpath:
+        logger.error("Document %s has no local file" % doc.pk)
+        return
+
     docdir, docname = os.path.split(docpath)
     ext = extension(docname)
-    newpath = os.path.join(docdir, "download.%s" % ext)
+    new_path = os.path.join(docdir, "download.%s" % ext)
 
     try:
-        doc.move_file(newpath)
+        do_rename(doc, new_path)
     except Exception as err:
         logger.error("Error while attempting rename:", err)
         return
