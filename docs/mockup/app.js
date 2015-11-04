bc97b9bb14b6cf20ef80e35974a6cf1347e3fd5a
Edwin Chalumeau <edwinchalumeau@outlook.com>
2015-11-03 19:42:05 -0500

    Revert "More work on implementing redesign"
    This reverts commit 272a10d4e6f4a7835fbd01649592d400daa8dbde.

--- docs/mockup/app.js
+++ docs/mockup/app.js
@@ -7,24 +7,17 @@ App = {
         window.map = map;
 
         map.addLayer(L.tileLayer("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"));
-        map.fitBounds([[42.4190212708995, -71.07150077819824],
-                       [42.372305415983895, -71.13570213317871]]);
+        map.fitBounds([[42.42009843116784, -71.05768203735352],
+                       [42.370720143531976, -71.14445686340332]]);
         $.getJSON("https://raw.githubusercontent.com/cityofsomerville/geodata/master/somerville.geojson")
             .done(function(features) {
                 map.addLayer(
                     L.geoJson(features,
                               {style: {
                                   stroke: 0.5,
-                                  color: "#EB4E12",
+                                  color: "orange",
                                   fill: false
                               }}));
             });
-        $("#main_activated").on("change", function(e) {
-            if (this.checked) {
-                $(document.body).addClass("main");
-            } else {
-                $(document.body).removeClass("main");
-            }
-        });
     }
-};
+}
