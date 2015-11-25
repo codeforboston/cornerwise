8dec7babd8fc23a08d8d58c2d2c60f8e97012d05
brisandbeans <bdougsand+cfb@gmail.com>
2015-11-24 22:48:53 -0500

    Minor changes to project files

--- server/project/views.py
+++ server/project/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db.models import Q
 from django.forms.models import model_to_dict
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 
 from shared.request import make_response
 
