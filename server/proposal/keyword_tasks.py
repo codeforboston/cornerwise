9eef099660c0f042e2bfe6b0ee55d052ff183291
brisandbeans <bdougsand+cfb@gmail.com>
2015-11-28 12:11:04 -0500

    Tasks for keyword analysis (work in progress)

--- server/proposal/keyword_tasks.py
+++ server/proposal/keyword_tasks.py
@@ -1,9 +1,37 @@
-import redis
+import datetime, redis
 
 from cornerwise import celery_app
 
 from . import idf, keywords
 from .models import Attribute
+
+# For testing:
+def add_full_text(r, doc):
+    terms = keywords.keywords(doc.get_text())
+    idf.add_document(r, terms, doc.pk)
+
+
+def add_full_docs(docs):
+    r = redis.StrictRedis()
+    pipe = r.pipeline(transaction=False)
+
+    for doc in docs:
+        key = "keywords:" + doc.pk + ":added"
+        if r.get(key):
+            continue
+        if doc.fulltext:
+            add_full_text(r, doc)
+            pipe.set(key, datetime.now().stamp())
+
+    pipe.execute()
+
+
+def top_terms(doc):
+    r = redis.StrictRedis()
+    terms = keywords.keywords(doc.get_text())
+    sorted_terms = idf.sorted_terms(r, terms)
+    return [t[0] for t in sorted_terms]
+
 
 celery_app.task(name="proposal.build_counts")
 def build_counts(handles=[]):
@@ -14,23 +42,7 @@ def build_counts(handles=[]):
         att_keywords = keywords.keywords(text)
         idf.add_document(r, att_keywords, att.pk)
 
+
 celery_app.task(name="proposal.analyze_text")
 def analyze_text(doc):
-    pass
-
-# For testing:
-def add_full_text(r, doc):
-    terms = keywords.keywords(doc.get_text())
-    idf.add_document(r, terms, doc.pk)
-
-def add_full_docs(docs):
-    r = redis.StrictRedis()
-    for doc in docs:
-        if doc.fulltext:
-            add_full_text(r, doc)
-
-def top_terms(doc):
-    r = redis.StrictRedis()
-    terms = keywords.keywords(doc.get_text())
-    sorted_terms = idf.sorted_terms(r, terms)
-    return [t[0] for t in sorted_terms]
+    att_keywords = keywords.keywords(doc.get_text())
