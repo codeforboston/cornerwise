dad8760bf909ee7f1f64ee491a695d01300f22b4
brisandbeans <bdougsand+cfb@gmail.com>
2015-12-29 23:15:28 -0500

    History events

--- client/scripts/src/routes.js
+++ client/scripts/src/routes.js
@@ -1,11 +1,5 @@
-define(["backbone"], function(B) {
+define(["backbone", "underscore", "utils"], function(B, _, $u) {
     var dispatcher = _.clone(B.Events);
-
-    var AppRouter = B.Router.extend({
-        routes: {
-            "details/:id": "details"
-        }
-    });
 
     var appRouter;
 
@@ -22,10 +16,43 @@ define(["backbone"], function(B) {
             return dispatcher;
         },
 
-        init: function() {
-            appRouter = new AppRouter();
+        setHashState: function(o, quiet) {
+            window.location.hash = $u.encodeQuery(o);
 
-            B.history.start();
+            if (!quiet)
+                this.dispatchEvents(o);
+        },
+
+        setHashKey: function(k, v, quiet) {
+            var hashObject = $u.decodeQuery(window.location.hash);
+
+            hashObject[k] = v;
+            this.setHashState(hashObject, quiet);
+        },
+
+        triggerHashState: function(o) {
+            o = o || $u.decodeQuery(B.history.getHash());
+            dispatcher.trigger("hashState", o);
+        },
+
+        onStateChange: function(cb) {
+            this.getDispatcher().on("hashState", function(state) {
+                cb(state);
+            });
+        },
+
+        init: function() {
+            this.triggerHashState();
+
+            var self = this;
+            B.history.start({hashChange: true});
+            B.history.handlers.push({
+                route: {test: _.constant(true)},
+                callback: function(fragment) {
+                    var o = $u.decodeQuery(fragment);
+                    self.triggerHashState(o);
+                }
+            });
 
             return appRouter;
         }
