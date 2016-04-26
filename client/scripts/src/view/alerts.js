5219931c1ef7454aa2e0285bd2cf907352edf36c
Brian Sanders <bdougsand+cfb@gmail.com>
2016-04-26 06:17:22 -0400

    Changed alerts behavior and appearance

--- client/scripts/src/view/alerts.js
+++ client/scripts/src/view/alerts.js
@@ -1,24 +1,47 @@
 define(["backbone", "jquery", "underscore"],
        function(B, $, _) {
-           var alertElement = $("#alert");
+           var alertElement = $("#alert"),
+               messages = [],
+               lastMessage;
+
+           function showMessage(message) {
+               if (_.isString(message)) {
+                   message = {text: message,
+                              className: ""};
+               }
+
+               alertElement.html(message.text)
+                   .addClass("displayed " + message.className);
+           }
+
+           function advance() {
+               if (lastMessage && lastMessage.className)
+                   alertElement.removeClass(lastMessage.className);
+
+               if (messages.length) {
+                   lastMessage = messages.shift();
+                   showMessage(lastMessage);
+               } else {
+                   alertElement.removeClass("displayed");
+                   lastMessage = null;
+               }
+           }
+
+           function start() {
+               if (!lastMessage) advance();
+           }
+
+           alertElement.click(function() {
+               advance();
+           });
 
            return {
                show: function(message, className) {
-                   if (_.isString(message)) {
-                       message = {text: message,
-                                  className: className || ''};
-                   }
+                   messages.push({text: message,
+                                  className: className || ""});
+                   start();
 
-                   alertElement
-                       .text(message.text)
-                       .addClass("displayed " + message.className)
-                       .delay(message.delay || 2000)
-                       .queue(function(next) {
-                           $(this)
-                               .removeClass("displayed " + message.className);
-
-                           next();
-                       });
+                   return alertElement;
                }
            };
        });
