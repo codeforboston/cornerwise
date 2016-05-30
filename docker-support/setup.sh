99fbb83ab1b28b412d6c08670839bdc85e5af0b7
Brian Sanders <bdougsand+cfb@gmail.com>
2016-05-30 02:32:50 +0000

    Cleaned up Docker image setup scripts
    - Parcel importing no longer happens as part of the image build process,
      since that added a build-time dependency on Postgres.

--- docker-support/setup.sh
+++ docker-support/setup.sh
@@ -7,6 +7,3 @@ apt-get install -y xpdf-utils
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
 pip3 install -r /support/requirements.txt
-
-sh /support/export_parcels.sh
-rm -r /shapefiles
