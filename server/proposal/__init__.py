4f2fa4e43b314c9d05e0b9b9e73641463c16a9cb
Brian Sanders <bdougsand+cfb@gmail.com>
2016-05-30 04:16:12 +0000

    Set up the proposal tasks on app startup

--- server/proposal/__init__.py
+++ server/proposal/__init__.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
 
+
 class ProposalConfig(AppConfig):
     name = "proposal"
 
     def ready(self):
         # Register tasks with Celery:
         from . import tasks
+        tasks.set_up_hooks()
