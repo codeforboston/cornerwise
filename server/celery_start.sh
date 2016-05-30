20c1bc8aa9fc8835b3de83defc77e26ac94a3286
Brian Sanders <bdougsand+cfb@gmail.com>
2016-05-30 02:36:29 +0000

    Create a separate Celery container
    - The celery container uses the 'cornerwise' image, since they share all
      the same dependencies.
    
    - celery_start.sh script runs when the container starts
    
    - Automatically start celery beat in production, but not in development
    
    - Tag the cornerwise container 'dev' when running in development

---
+++ server/celery_start.sh
@@ -0,0 +1,12 @@
+cd $(dirname "${BASH_SOURCE[0]}")
+
+export C_FORCE_ROOT=1
+
+if [ "$APP_MODE" = "production" ]; then
+    # Only run tasks automatically in production:
+    celery -A cornerwise beat --detach
+else
+    celery_opts=" --loglevel=INFO"
+fi
+
+celery -A cornerwise worker $celery_opts
