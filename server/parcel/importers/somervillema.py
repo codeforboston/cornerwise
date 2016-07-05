d18421837ecb0b772f6a77c3f2834640393e6e62
Brian Sanders <bdougsand+cfb@gmail.com>
2016-07-05 19:04:44 +0000

    Minor changes to Somerville parcel importer

--- server/parcel/importers/somervillema.py
+++ server/parcel/importers/somervillema.py
@@ -2,12 +2,11 @@ from django.contrib.gis.gdal import SpatialReference
 from django.contrib.gis.utils import LayerMapping
 from django.db import IntegrityError
 import dbfread
-import logging
 import os
 
 from parcel.models import Parcel
 
-logger = logging.getLogger(__name__)
+name = "Somerville, MA"
 
 # Note: This is the WKT from M274TaxPar.prj, with one important difference. The
 # given WKT specifies Lambert_Conformal_Conic, which is ambiguous. The intended
@@ -15,7 +14,7 @@ logger = logging.getLogger(__name__)
 srs_wkt = 'PROJCS["NAD_1983_StatePlane_Massachusetts_Mainland_FIPS_2001",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["False_Easting",200000.0],PARAMETER["False_Northing",750000.0],PARAMETER["Central_Meridian",-71.5],PARAMETER["Standard_Parallel_1",41.71666666666667],PARAMETER["Standard_Parallel_2",42.68333333333333],PARAMETER["Latitude_Of_Origin",41.0],UNIT["Meter",1.0]]'
 
 
-def run():
+def import_shapes(logger):
     srs = SpatialReference(srs_wkt)
     lm = LayerMapping(Parcel, "/data/shapefiles/M274TaxPar.shp",
                       {"shape_leng": "SHAPE_Leng",
@@ -33,7 +32,7 @@ def run():
     lm.save(strict=True)
 
 
-def add_assessor_data(file_name="M274Assess.dbf"):
+def add_assessor_data(logger, file_name="M274Assess.dbf"):
     logger.info("Adding assessor data...")
     # This could be done more efficiently, but it only needs to run once.
     path = os.path.join("/data/shapefiles", file_name)
@@ -55,3 +54,7 @@ def add_assessor_data(file_name="M274Assess.dbf"):
                         continue
         except Parcel.DoesNotExist:
             continue
+
+def run(logger=None):
+    import_shapes(logger)
+    add_assessor_data(logger)
