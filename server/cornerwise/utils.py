161e0d3c7ce44fbbed1724e37d694d6702c9d73c
Brian Sanders <bdougsand+cfb@gmail.com>
2016-07-07 03:09:16 +0000

    Don't attempt to absolutize URL

--- server/cornerwise/utils.py
+++ server/cornerwise/utils.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 
 from datetime import datetime
+import re
 
 
 protocol = "https" if settings.IS_PRODUCTION else "http"
 
 def make_absolute_url(path):
+    if re.match(r"^https?://", path):
+        return path
+
     return "{protocol}://{domain}{path}".format(protocol=protocol,
                                                 domain=settings.SERVER_DOMAIN,
                                                 path=path)
