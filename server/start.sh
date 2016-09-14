0154ba15864237c7b3e242b2115b71fbd5a31b2c
Brian Sanders <bdougsand+cfb@gmail.com>
2016-09-14 17:49:42 +0000

    More changes to how celery is started
    - Added a link from the `cornerwise` container to the `celery` container
      in production
    
    - Added a `prod2` version of the docker-compose file that does not start
      a separate celery container
    
    - Modified the server start script to start celery inside the cornerwise
      container only if there is no link to the `celery` container,
      independent of what the `APP_MODE` variable says

--- server/start.sh
+++ server/start.sh
@@ -32,16 +32,16 @@ PYTHON_BIN=$(which python3 || which python)
 echo "Applying any outstanding migrations"
 $PYTHON_BIN $APP_ROOT/manage.py migrate
 
-if [ "$APP_MODE" = "production" ]; then
-    gunicorn -b "0.0.0.0:$APP_PORT" cornerwise.wsgi
-else
+rm /tmp/*.pid
+
+# Start celery in the background of this container if there is not a linked
+# container running with the name 'celery'.
+if ! getent hosts celery; then
     export C_FORCE_ROOT=1
 
     if ((CELERY_MONITOR)); then
         celery_opts="-E"
     fi
-
-    rm /tmp/*.pid
 
     $PYTHON_BIN manage.py celery -A $APP_NAME beat \
                 --pidfile=/tmp/celerybeat.pid &
@@ -49,5 +49,10 @@ else
                 --pidfile=/tmp/celery.pid \
                 --autoscale=$CELERY_MAX_WORKERS,$CELERY_MIN_WORKERS \
                 $celery_opts &
+fi
+
+if [ "$APP_MODE" = "production" ]; then
+    gunicorn -b "0.0.0.0:$APP_PORT" cornerwise.wsgi
+else
     $PYTHON_BIN manage.py runserver 0.0.0.0:$APP_PORT
 fi
