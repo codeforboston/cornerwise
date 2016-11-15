cf4814379d063e68218518fb5f511493174c0997
Brian Sanders <bdougsand+cfb@gmail.com>
2016-11-14 20:15:36 -0500

    User view decorators

---
+++ server/user/utils.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import login, logout
+from django.contrib import messages
+
+
+def not_logged_in(request):
+    return render(request, "user/not_logged_in.djhtml")
+
+
+def with_user(view_fn):
+    def wrapped_fn(request, *args, **kwargs):
+        if not request.user.is_anonymous():
+            return view_fn(request, request.user, *args, **kwargs)
+        else:
+            try:
+                user = authenticate(pk=request.GET["uid"],
+                                    token=request.GET["token"])
+                if user:
+                    messages.success(request, "Welcome back!")
+                    login(request, user)
+                    return view_fn(request, user, *args, **kwargs)
+            except KeyError:
+                messages.error(request,
+                               "You must be logged in to view that page.")
+
+            return not_logged_in(request)
+
+    return wrapped_fn
+
