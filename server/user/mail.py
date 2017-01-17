2e721b4c4c3635e61202ae1591775b74ab2d447b
Brian Sanders <bdougsand+cfb@gmail.com>
2017-01-16 23:41:01 -0500

    Reorganized users modules

---
+++ server/user/mail.py
@@ -0,0 +1,92 @@
+import sendgrid
+
+from celery.utils.log import get_task_logger
+from django.conf import settings
+from django.template import TemplateDoesNotExist
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+
+if getattr(settings, "SENDGRID_API_KEY", None):
+    SG = sendgrid.SendGridAPIClient(
+        apikey=settings.SENDGRID_API_KEY, raise_errors=True)
+else:
+    SG = None
+
+
+def has_name(u):
+    return u.first_name and u.last_name
+
+
+def send(user, subject, template_name, context={}, content=None):
+    """
+    Send an email to a user. If there is a SendGrid template id configured for
+    the given template name, create substitutions from `context` so that `-key`
+    in the template is replaced by the value of `key` in `context`.
+
+    If there is no such SendGrid template, falls back to using a Django
+    template in <templates>/email.
+    """
+    try:
+        template_id = settings.SENDGRID_TEMPLATES[template_name]
+    except:
+        # Fall back to Django templates
+        return send_template(user, subject, template_name, context)
+
+    substitutions = {"-{}-".format(k): str(v) for k, v in context.items()}
+    substitutions["-user-"] = user.profile.addressal
+    substitutions["-unsubscribe_link-"] = user.profile.unsubscribe_url
+
+    data = {
+        "personalizations": [{
+            "to": [{
+                "email": user.email
+            }],
+            "substitutions": substitutions,
+            "subject": subject,
+        }],
+        "from": {
+            "email": settings.EMAIL_ADDRESS,
+            "name": settings.EMAIL_NAME
+        },
+        "template_id": template_id
+    }
+
+    if content:
+        data["content"] = [{"type": "text/html", "value": content}]
+
+    try:
+        SG.client.mail.send.post(request_body=data)
+    except request.HTTPError as err:
+        return (False, err)
+    else:
+        logger.info("Email sent to {}".format(user.email))
+        return True
+
+
+def send_template(user, subject, template_name, context={}):
+    context = context.copy()
+    context["user"] = user.profile.addressal()
+    html = render_to_string("email/" + template_name + ".djhtml", context)
+    try:
+        text = render_to_string("email/" + template_name + ".djtxt", context)
+    except TemplateDoesNotExist:
+        text = strip_tags(html)
+
+    if SG:
+        message = sendgrid.Mail(
+            subject=subject,
+            html=html,
+            text=text,
+            from_email=settings.EMAIL_ADDRESS)
+        if has_name(user):
+            message.add_to("{first} {last} <{email}>".format(
+                first=user.first_name, last=user.last_name, email=user.email))
+        else:
+            message.add_to(user.email)
+        status, msg = SG.send(message)
+        logger.info("Sent '%s' email to %s (status %i)", template_name,
+                    user.email, status)
+    else:
+        logger.info("SendGrid not available. Generated email: %s", html)
+
--- server/user/tasks.py
+++ server/user/tasks.py
@@ -1,108 +1,20 @@
 from datetime import datetime, timedelta
 from urllib import request
 
-import sendgrid
-
 from celery.utils.log import get_task_logger
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-from django.template import TemplateDoesNotExist
+from django.dispatch import receiver
 from django.template.loader import render_to_string
-from django.utils.html import strip_tags
 
 from cornerwise import celery_app
 from cornerwise.utils import make_absolute_url
 
 from .models import Subscription, UserProfile
-from .changes import summarize_subscription_updates
-# from proposal.models import Proposal
-# from proposal.query import build_proposal_query
+from . import changes, mail
 
 logger = get_task_logger(__name__)
-
-if getattr(settings, "SENDGRID_API_KEY", None):
-    SG = sendgrid.SendGridAPIClient(
-        apikey=settings.SENDGRID_API_KEY, raise_errors=True)
-else:
-    SG = None
-
-
-def has_name(u):
-    return u.first_name and u.last_name
-
-
-def send_mail(user, subject, template_name, context={}, content=None):
-    """
-    Send an email to a user. If there is a SendGrid template id configured for
-    the given template name, create substitutions from `context` so that `-key`
-    in the template is replaced by the value of `key` in `context`.
-
-    If there is no such SendGrid template, falls back to using a Django
-    template in <templates>/email.
-    """
-    try:
-        template_id = settings.SENDGRID_TEMPLATES[template_name]
-    except:
-        # Fall back to Django templates
-        return send_mail_template(user, subject, template_name, context)
-
-    substitutions = {"-{}-".format(k): str(v) for k, v in context.items()}
-    substitutions["-user-"] = user.profile.addressal
-    substitutions["-unsubscribe_link-"] = user.profile.unsubscribe_url
-
-    data = {
-        "personalizations": [{
-            "to": [{
-                "email": user.email
-            }],
-            "substitutions": substitutions,
-            "subject": subject,
-        }],
-        "from": {
-            "email": settings.EMAIL_ADDRESS,
-            "name": settings.EMAIL_NAME
-        },
-        "template_id": template_id
-    }
-
-    if content:
-        data["content"] = [{"type": "text/html", "value": content}]
-
-    try:
-        SG.client.mail.send.post(request_body=data)
-    except request.HTTPError as err:
-        return (False, err)
-    else:
-        logger.info("Email sent to {}".format(user.email))
-        return True
-
-
-def send_mail_template(user, subject, template_name, context={}):
-    context = context.copy()
-    context["user"] = user.profile.addressal()
-    html = render_to_string("email/" + template_name + ".djhtml", context)
-    try:
-        text = render_to_string("email/" + template_name + ".djtxt", context)
-    except TemplateDoesNotExist:
-        text = strip_tags(html)
-
-    if SG:
-        message = sendgrid.Mail(
-            subject=subject,
-            html=html,
-            text=text,
-            from_email=settings.EMAIL_ADDRESS)
-        if has_name(user):
-            message.add_to("{first} {last} <{email}>".format(
-                first=user.first_name, last=user.last_name, email=user.email))
-        else:
-            message.add_to(user.email)
-        status, msg = SG.send(message)
-        logger.info("Sent '%s' email to %s (status %i)", template_name,
-                    user.email, status)
-    else:
-        logger.info("SendGrid not available. Generated email: %s", html)
 
 
 @celery_app.task(name="user.send_updates")
@@ -110,12 +22,15 @@ def send_subscription_updates(user_id, updates):
     user = User.objects.get(pk=user_id)
     updates_html = render_to_string("email/changes.djhtml",
                                     {"changes": updates})
-    send_mail_template(user, "Cornerwise: New Updates", "updates",
+    mail.send_template(user, "Cornerwise: New Updates", "updates",
                        {"updates": updates_html})
 
 
 @celery_app.task(name="user.send_user_key")
-def send_user_key(user_id):
+def send_user_key(user_id, created):
+    if not created:
+        return
+
     profile = UserProfile.objects.get(user_id=user_id)
     if not profile.token:
         profile.generate_token()
@@ -124,29 +39,13 @@ def send_user_key(user_id):
 
     # Render HTML and text templates:
     context = {"confirm_url": make_absolute_url(profile.confirm_url)}
-    send_mail(profile.user, "Cornerwise: Welcome", "welcome", context)
-
-
-@celery_app.task(name="user.resend_user_key")
-def resend_user_key(user_id):
-    profile = UserProfile.objects.get(user_id=user_id)
-    if not profile.token:
-        profile.generate_token()
-
-    send_mail(profile.user, "Cornerwise: Your Account", "login_link",
-              {"user": profile.user})
+    mail.send(profile.user, "Cornerwise: Welcome", "welcome", context)
 
 
 @celery_app.task()
 def send_deactivation_email(user_id):
     user = User.objects.get(pk=user_id)
-    send_mail(user, "Cornerwise: Account Disabled", "account_deactivated")
-
-
-@celery_app.task()
-def send_reset_email(user_id):
-    user = User.objects.get(pk=user_id)
-    send_mail(user, "Cornerwise: Login Reset", "account_reset")
+    mail.send(user, "Cornerwise: Unsubscribed", "account_deactivated")
 
 
 @celery_app.task()
@@ -182,7 +81,7 @@ def send_subscription_confirmation_email(sub_id):
         "old_minimap_src": existing.minimap_src,
         "confirmation_link": make_absolute_url(subscription.confirm_url)
     }
-    send_mail(user, "Cornerwise: Confirm New Subscription",
+    mail.send(user, "Cornerwise: Confirm New Subscription",
               "replace_subscription", context)
 
 
@@ -201,30 +100,20 @@ def send_notifications(subscription_ids=None, since=None):
         since = datetime.now() - timedelta(days=7)
 
     for subscription in subscriptions:
-        updates = summarize_subscription_updates(subscription, since)
+        updates = changes.summarize_subscription_updates(subscription, since)
         if updates:
             send_subscription_updates(subscription.user, updates)
 
 
 # Database hooks:
+@receiver(post_save, sender=UserProfile, dispatch_uid="send_confirmation_email")
 def user_profile_created(**kwargs):
-    if kwargs["created"]:
-        profile = kwargs["instance"]
-        send_user_key.delay(profile.user_id)
+    profile = kwargs["instance"]
+    send_user_key.delay(kwargs["instance"].user_id, kwargs["created"])
 
 
+@receiver(post_save, sender=Subscription, dispatch_uid="send_subscription_confirmation_email")
 def subscription_created(**kwargs):
     if kwargs["created"]:
         subscription = kwargs["instance"]
-        send_subscription_confirmation_email.delay(subscription.id)
-
-
-def set_up_hooks():
-    post_save.connect(
-        user_profile_created,
-        UserProfile,
-        dispatch_uid="send_confirmation_email")
-    post_save.connect(
-        subscription_created,
-        Subscription,
-        dispatch_uid="send_subscription_confirmation_email")
+        send_subscription_confirmation_email.delay(subscription.pk)
