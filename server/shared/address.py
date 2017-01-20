9d716e15d778e74b4ecf4e2cf194e14b38ccd876
Brian Sanders <bdougsand+cfb@gmail.com>
2017-01-19 22:49:57 -0500

    Foursquare API stuff

--- server/shared/address.py
+++ server/shared/address.py
@@ -13,15 +13,18 @@ subs = {
     "TERRACE": "TER"
 }
 
+
 def normalize_street(street):
     street = street.upper()
     pieces = re.split(r"[\s.]+", street)
     return " ".join(subs.get(p, p) for p in pieces if p)
 
+
 def normalize_number(num):
     m = re.match(r"\d+", num)
     if m:
         return m.group(0)
+
 
 def split_address(s):
     m = re.match(r"(\d+)(-\d+)?\s", s)
@@ -30,3 +33,15 @@ def split_address(s):
         street = s[m.end():]
 
         return number, street
+
+
+def normalize_address(s):
+    # Could try using 'select normalize_address()' from PostGIS, which is
+    # certainly more robust.
+    number, street = split_address(s)
+    return "{} {}".format(normalize_number(number),
+                          normalize_street(street))
+
+
+def same_address(addr1, addr2):
+    return normalize_address(addr1) == normalize_address(addr2)
