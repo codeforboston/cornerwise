2d0e4374fef96f0a4bfd7f57fe366e76f8d41d9e
Brian Sanders <bdougsand+cfb@gmail.com>
2017-01-22 20:31:56 -0500

    Pass lot size cutoffs to template

--- server/cornerwise/views.py
+++ server/cornerwise/views.py
@@ -2,9 +2,20 @@ from django.conf import settings
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+from parcel.models import LotQuantiles
+
+
+def get_lot_sizes():
+    q = LotQuantiles.objects.all()[0]
+    return {"small": round(q.small_lot * 43560),
+            "medium": round(q.medium_lot * 43560)}
+
+LOT_SIZES = get_lot_sizes()
+
 
 @ensure_csrf_cookie
 def index(request):
     return render(request, "index.djhtml",
                   {"google_key": settings.GOOGLE_BROWSER_API_KEY,
-                   "app_mode": settings.APP_MODE})
+                   "app_mode": settings.APP_MODE,
+                   "lot_sizes": LOT_SIZES})
