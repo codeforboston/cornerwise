9beea5c2866a4fafb4ee7c0d7a165926ea441453
Brian Sanders <bdougsand+cfb@gmail.com>
2017-02-19 08:08:59 -0500

    WIP: Use image colors to calculate colorfulness
    Could be used as an input to an "image interestingness" algorithm

--- server/scripts/vision.py
+++ server/scripts/vision.py
@@ -1,4 +1,6 @@
 import base64
+import itertools
+from math import sqrt
 
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials, ApplicationDefaultCredentialsError
@@ -72,6 +74,60 @@ def find_logo(response, full_width=None, full_height=None):
                 return logo
         elif logo["score"] > 0.80:
             return logo
+
+
+def rgb_to_xyz(r, g, b):
+    def convert(x):
+        p = x / 255
+        return 100 * (((p * 0.055) / 1.055)
+                      **2.4) if p > 0.04045 else (p / 12.92)
+
+    r = convert(r)
+    g = convert(g)
+    b = convert(b)
+
+    return (r * 0.4124 + g * 0.3576 + b * 0.1805,
+            r * 0.2126 + g * 0.7152 + b * 0.0722,
+            r * 0.0193 + g * 0.1192 + b * 0.9505)
+
+
+def xyz_to_lab(x, y, z):
+    sqrt_y = sqrt(y)
+    return (10 * sqrt_y, # L
+            17.5 * (((1.02 * x) - y) / sqrt_y), # a
+            7 * ((y - (0.847 * z)) / sqrt_y)) # b
+
+
+# See https://en.wikipedia.org/wiki/Colorfulness
+def perceived_saturation(color):
+    (x, y, z) = rgb_to_xyz(color["red"], color["green"], color["blue"])
+    (L, a, b) = xyz_to_lab(x, y, z)
+    a2_b2 = a*a + b*b
+    return 100 * sqrt(a2_b2) / sqrt(L*L + a2_b2)
+
+
+def colorfulness(response):
+    """
+    Calculate how colorful an image appears based on the perceived saturation
+    of its dominant colors.
+    """
+    props = response["imagePropertiesAnnotation"]
+    colors = props["dominantColors"]["colors"]
+    # Total pixel fraction
+    total = 0
+    total_sat = 0
+    for color in colors:
+        sat = perceived_saturation(color["color"])
+        frac = color["pixelFraction"]
+        total_sat += frac*sat
+        total += frac
+
+    return total_sat/total
+
+
+def simplify_labels(response):
+    return ((l["description"], l["score"])
+            for l in response["labelAnnotations"])
 
 
 def process_image(image_file):
