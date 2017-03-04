1428bbb45937f34c7c38c7a67e15e5e58d2e14c7
Brian Sanders <bdougsand+cfb@gmail.com>
2017-03-04 10:12:49 -0500

    Inbound mail: fix and exempt from CSRF protection

--- server/user/mail_parse_views.py
+++ server/user/mail_parse_views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 import logging
 
@@ -24,25 +26,33 @@ def forward_mail(username, from_email, text):
         else:
             users = [User.objects.get(username=username, is_staff=True)]
         send_fowarded_message(users, from_email, text)
+        return HttpResponse("OK: Sent", status=200)
     except User.DoesNotExist:
         logger.warn(f"Received email for unknown user '{username}' from {from_email}")
+        return HttpResponse("OK: Not sent", status=200)
 
 
 # When email is sent to @cornerwise.org, SendGrid parses it and posts the
 # reults to this view:
+@csrf_exempt
 def mail_inbound(request):
     """
     See https://sendgrid.com/docs/API_Reference/Webhooks/parse.html for
     documentation of the POST fields.
     """
     try:
-        email = request.POST["from"]
+        parse_key = settings.SENDGRID_PARSE_KEY
+        if parse_key and request.GET.get("key") != parse_key:
+            return HttpResponse("NOK", status=403)
+
+        from_email = request.POST["from"]
         body = request.POST["text"]
-        user = User.objects.get(email=email)
+        user = User.objects.get(email=from_email)
 
         (username, _) = request.POST["to"].split("@", 1)
         if username.lower() not in {"cornerwise", "noreply"}:
-            forward_mail(to, )
+            return forward_mail(to, from_email, body)
+
         lines = request.POST["text"].split("\n")
         for line in lines:
             command = line.strip().split(" ")[0]
