66c96de755e728532676ad8cda2cefd3d196900f
Brian Sanders <bdougsand+cfb@gmail.com>
2017-07-09 10:02:13 -0400

    Generate an absolute URL for unsubscribe link
    Addresses #263

--- server/user/mail.py
+++ server/user/mail.py
@@ -1,4 +1,13 @@
 from shared import mail
+from cornerwise.utils import make_absolute_url
+
+
+def _make_user_context(user, context):
+    context = context.copy() if context else {}
+    context["user"] = user.profile.addressal
+    context["unsubscribe_url"] = make_absolute_url(user.profile.unsubscribe_url)
+    return context
+
 
 def send(user, subject, template_name, context=None, content=None):
     """
@@ -9,8 +18,6 @@ def send(user, subject, template_name, context=None, content=None):
     If there is no such SendGrid template, falls back to using a Django
     template in <templates>/email.
     """
-    context = context or {}
-    context["user"] = user.profile.addressal
-    context["unsubscribe_url"] = user.profile.unsubscribe_url
 
-    return mail.send(user.email, subject, template_name, context)
+    return mail.send(user.email, subject, template_name,
+                     _make_user_context(user, context))
