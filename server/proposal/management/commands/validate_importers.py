3d1bcf532639ca57e53dcc23be08d5f4f2965e3a
Brian Sanders <bdougsand+cfb@gmail.com>
2017-10-20 00:14:59 -0400

    Improved ./manage.py validate_importers
    - Show all errors rather than the first one that produces an error
    
    - Consolidate issues that affect the same schema path (e.g., when many items
      share the same validation issue)
    
    - Syntax highlighting

--- server/proposal/management/commands/validate_importers.py
+++ server/proposal/management/commands/validate_importers.py
@@ -1,22 +1,39 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
+from collections import defaultdict
 from datetime import datetime, timedelta
 import json
 from urllib.request import urlopen
 
-from proposal.models import Importer
+from proposal.models import Importer, get_importer_schema
 
-import jsonschema
+from jsonschema import Draft4Validator
+
+
+def schema_path_str(path):
+    return "".join(f".{c}" if isinstance(c, str) else f"[{c}]" for c in path)
 
 
 class Command(BaseCommand):
     help = "Validate that the importers conform to the JSON schema"
 
     def handle(self, *args, **options):
-        now = datetime.now() - timedelta(days=30)
+        err = lambda m: self.stderr.write(self.style.ERROR(m) + "\n")
+        succ = lambda m: self.stdout.write(self.style.SUCCESS(m) + "\n")
+        when = datetime.now() - timedelta(days=30)
+        validator = Draft4Validator(get_importer_schema())
+
         for importer in Importer.objects.all():
-            try:
-                importer.validate(now)
-            except jsonschema.exceptions.ValidationError as err:
-                self.stdout.write(f"Error in {importer}: {err}")
+            errors = defaultdict(list)
+            for error in validator.iter_errors(importer.updated_since(when)):
+                errors[schema_path_str(error.absolute_schema_path)].append(error)
+
+            if errors:
+                err(f"{len(errors)} error(s) in {importer}:")
+                for path, errs in errors.items():
+                    error = errs[0]
+                    repeats = f" ({len(errs)} repeats)" if len(errs) > 1 else ""
+                    err(f"at {path}{repeats}:\n{error.message}\n")
+            else:
+                succ(f"{importer} validated successfully!")
