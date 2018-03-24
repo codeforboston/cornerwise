7d37906531260911f5aa3e447a246e12f7600e4c
Brian Sanders <bdougsand+cfb@gmail.com>
2018-03-24 11:36:51 -0400

    Admin view for looking at recent task log output

--- server/cornerwise/admin_urls.py
+++ server/cornerwise/admin_urls.py
@@ -5,5 +5,6 @@ from shared import admin_views, admin
 
 urlpatterns = [
     url(r"^celery_log", admin_views.celery_logs),
+    url(r"^task_logs", admin_views.task_logs, name="task_logs"),
     url(r"^", admin.cornerwise_admin.urls)
 ]
--- server/shared/admin_views.py
+++ server/shared/admin_views.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
-from utils import read_n_from_end
+from utils import read_n_from_end, Redis
 
 
 is_superuser = user_passes_test(lambda user: user.is_superuser, "/admin")
+
+
+def get_task_logs(task_ids):
+    with Redis.pipeline() as p:
+        for task_id in task_ids:
+            p.lrange(f"cornerwise:task_log:{task_id}", 0, 100)
+        logs = p.execute()
+
+    return dict(zip(task_ids, [map(bytes.decode, reversed(l))
+                               for l in logs]))
 
 
 @is_superuser
@@ -16,3 +26,11 @@ def celery_logs(request):
     return render(request, "admin/log_view.djhtml",
                   {"log_name": "Celery Tasks Log",
                    "lines": log_lines})
+
+
+@is_superuser
+def task_logs(request):
+    task_ids = request.GET.getlist("task_id")
+    return render(request, "admin/task_logs.djhtml",
+                  {"logs": get_task_logs(task_ids)})
+
