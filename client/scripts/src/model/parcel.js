047423b59c341cd724c7402650d738dda71d7f35
Brian Sanders <bdougsand+cfb@gmail.com>
2018-04-16 22:05:05 -0400

    Do some preprocessing on parcel attributes

--- client/scripts/src/model/parcel.js
+++ client/scripts/src/model/parcel.js
@@ -1,12 +1,33 @@
 define(["backbone", "underscore"],
        function(B, _) {
+           var propTypes = {
+               BLD_AREA: parseFloat,
+               LOT_SIZE: parseFloat,
+               YEAR_BUILT: parseInt,
+               UNITS: parseInt,
+               STORIES: parseInt,
+               NUM_ROOMS: parseInt,
+               STYLE: null,
+               USE_CODE: null,
+               ZONING: null
+           };
+
+           function toPropName(k) {
+               return _.map(k.split(/_+/), function(piece, i) {
+                   return (i ? piece[0] : "") + piece.substring(i ? 1 : 0).toLowerCase();
+               }).join("");
+           }
+
            return B.Model.extend({
-               initialize: function() {
-
-               },
-
                parse: function(response) {
-                   return _.extend(response.properties, {
+                   var props = response.properties;
+
+                   _.forEach(propTypes, function(fn, k, i) {
+                       if (props[k])
+                           props[toPropName(k)] = fn ? fn(props[k]) : props[k];
+                   });
+
+                   return _.extend(props, {
                        geom: {coordinates: response.coordinates,
                               type: response.type}
                    });
