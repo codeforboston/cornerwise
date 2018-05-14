2cd75830efda5dfae07e8d0e85f499af269895b7
Brian Sanders <bdougsand+cfb@gmail.com>
2018-05-14 10:03:43 -0400

    Lot quantiles are grouped by town_id; serve the correct ones

--- server/cornerwise/views.py
+++ server/cornerwise/views.py
@@ -8,24 +8,27 @@ from parcel.models import LotQuantiles
 
 
 def get_lot_sizes():
-    try:
-        q = LotQuantiles.objects.all()[0]
-        return {
-            "small": round(q.small_lot * 43560),
-            "medium": round(q.medium_lot * 43560)
+    return {
+        town.town_id: {
+            "small": round(town.small_lot * 43560),
+            "medium": round(town.medium_lot * 43560)
         }
-    except IndexError:
-        return {"small": 5000, "medium": 10000}
+        for town in LotQuantiles.objects.all()
+    }
 
 
 LOT_SIZES = None
 
 
-def lot_sizes():
+def lot_sizes(town_id):
     global LOT_SIZES
     if not LOT_SIZES:
         LOT_SIZES = get_lot_sizes()
-    return LOT_SIZES
+
+    try:
+        return LOT_SIZES[town_id]
+    except KeyError:
+        return { "small": 5000, "medium": 10000 }
 
 
 @ensure_csrf_cookie
@@ -33,7 +36,7 @@ def index(request):
     return render(request, "index.djhtml", {
         "google_key": settings.GOOGLE_BROWSER_API_KEY,
         "production": settings.IS_PRODUCTION,
-        "lot_sizes": lot_sizes(),
+        "lot_sizes": lot_sizes(request.site_config.town_id),
         "preload_data": json.dumps({}),
         "js_filename": settings.JS_FILENAME,
         "css_filename": settings.CSS_FILENAME
