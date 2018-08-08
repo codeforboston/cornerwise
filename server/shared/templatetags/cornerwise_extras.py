b942ea7c1a05e1b5f8946fb57bdd16dca6323dfe
Brian Sanders <bdougsand+cfb@gmail.com>
2018-08-08 12:04:09 -0400

    pretty_print template filter

--- server/shared/templatetags/cornerwise_extras.py
+++ server/shared/templatetags/cornerwise_extras.py
@@ -1,6 +1,9 @@
 from django import template
 from django.urls import reverse
 
+import io
+import pprint
+
 from utils import make_absolute_url
 
 register = template.Library()
@@ -15,3 +18,11 @@ def absolutize(context, url):
 def absolute_url(context, url_name, *args, **kwargs):
     url = reverse(url_name, args=args, kwargs=kwargs)
     return make_absolute_url(url, context.get("hostname"))
+
+
+@register.filter()
+def pretty_print(x):
+    with io.StringIO() as buff:
+        pprint.pprint(x, buff)
+        buff.seek(0)
+        return buff.read()
